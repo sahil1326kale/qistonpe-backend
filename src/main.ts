ab2438f1e85c1e559cf4669b0a8c63da96ae2efb
Sahil Kale <sahil1326k@gmail.com>
2026-01-18 21:59:11 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
+
